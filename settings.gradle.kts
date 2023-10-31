a994b10c4a4e89caf365a172bf1d8cfadadad136
Morris <holymoai@gmail.com>
2023-10-31 12:13:18 +0900

    Fix: Nexus repository 연동

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 rootProject.name="NWAppLogger"
+include(":nwapplogger")
